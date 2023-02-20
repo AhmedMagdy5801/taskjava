9b8d00332ecd49ecf60738b0348e26798fc6ae2e
AhmedMagdy5801 <124633235+AhmedMagdy5801@users.noreply.github.com>
2023-02-20 22:48:14 +0200

    Add files via upload

---
+++ taskjava/Task3.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.task3;
+
+/**
+ *
+ * @author DELL
+ */
+public class Task3 {
+
+    public static void main(String[] args) {
+          if (job.recruting== true) {
+            System.out.println("lucky");
+            
+        } else {
+            System.out.println("unlucky");
+        }
+        programmer obj = new programmer("Ahmed",20);
+        
+        if (job.recruting== true) {
+            System.out.println("lucky");
+            
+        } else {
+            System.out.println("unlucky");
+        }
+    }
+}
---
+++ taskjava/programmer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.task3;
+
+/**
+ *
+ * @author DELL
+ */
+public class programmer extends employee {
+     public programmer(String name,int age){
+          super();
+        
+        System.out.println("Name is " +name  );
+        System.out.println("Age is " +age );
+       job.recruting = true;
+}
+    
+}
